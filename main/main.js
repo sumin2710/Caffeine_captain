2414675ba9c8f2ec58f4bef61a6cc68d4d306a66
choobao <154485888+choobao@users.noreply.github.com>
2024-01-10 22:33:18 +0900

    페이지 디테일save

---
+++ main/main.js
@@ -0,0 +1,38 @@
+const url = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTYzYjdjMWU5ZTIyZDAwM2ZjNjc5OWZlNjhmZWIxZCIsInN1YiI6IjY1OGViOWUxY2EwZTE3MDhmNGJhNzVkMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h4S8wIqJ0DxjJ9506Hrj3F5AfsavVajdG9cVybz_RnM"
+  }
+};
+
+fetch(url, options)
+  .then((response) => response.json())
+  .then((response) => {
+    const rows = response["results"];
+
+    rows.forEach((data) => {
+      const title = data["title"];
+      const id = data["id"];
+      const overview = data["overview"];
+      const vote = data["vote_average"];
+      const path = data["poster_path"];
+      console.log(response);
+
+      let temp_html = `
+      <div class="card mb-3" id="card-${id}">
+      <img src="https://image.tmdb.org/t/p/w300${path}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">${overview}</p>
+        <p class="card-text"><small class="text-body-secondary">(${vote}/10)</small></p>
+      </div>
+    </div>`;
+
+      //1. 바닐라 JS로 api 연동, html 붙여넣기
+      let cardElement = document.getElementById("card");
+      cardElement.innerHTML += temp_html;
+    });
+  });
