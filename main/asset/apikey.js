72edfcecf693fccda09533f1c1a0a501a67d1f7b
승원 <153504415+bobbodaddeuk@users.noreply.github.com>
2024-01-15 21:04:38 +0900

    Delete main/asset directory

--- main/asset/apikey.js
+++
@@ -1,9 +0,0 @@
-export { options };
-const options = {
-  method: "GET",
-  headers: {
-    accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjlhMmI2MjBiNjMwYjViM2RmYWEyMzBhNDIzODFiMCIsInN1YiI6IjY1OTNjZGE0Y2U0ZGRjNmU2NDdkZGIyOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4CSbF2T53qjlLENtBlffmhp-YxvcN7oE5otcgSdDpHc",
-  },
-};
